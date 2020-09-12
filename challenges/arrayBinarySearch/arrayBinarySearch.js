770fab72552cb4219e4ac2a29fe4fb9110834f1f
hussein <hussein66253@icloud.com>
2020-09-12 19:07:56 +0300

    done

--- challenges/arrayBinarySearch/arrayBinarySearch.js
+++ challenges/arrayBinarySearch/arrayBinarySearch.js
@@ -1,12 +1,31 @@
 'use strict'
-function BinarySearch  (arr, key){
-    for (let i =0 ; i<= arr.lengt ; i++){
-        if (arr[i] === key){
-            return 1;
-        } 
-       
+function BinarySearch(arr, x) {
+
+    let start = 0, end = arr.length - 1;
+
+
+
+    while (start <= end) {
+        for (let i = 0; i <= arr.length; i++) {
+
+            let mid = Math.floor((start + end) / 2);
+
+            if (arr[mid] === x) return arr[mid][i];
+
+            else if (arr[mid] < x)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
     }
-     return -1;
+
+    return -1;
+
 }
-console.log(BinarySearch([1,2,3,4,5,6],6))
-console.log(BinarySearch([1,2,3,4,5,6],54))
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+BinarySearch(arr, 1)
+BinarySearch(arr, 111)
+
+// Time Complexity: O(logN)
