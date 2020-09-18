7269e064092b5f499984c6251209e964fa0c16e8
hussein <hussein66253@icloud.com>
2020-09-18 15:58:58 +0300

    fixed the BinarySearch

--- challenges/arrayBinarySearch/arrayBinarySearch.js
+++ challenges/arrayBinarySearch/arrayBinarySearch.js
@@ -1,31 +1,19 @@
 #!/usr/bin/env node
 'use strict';
-function BinarySearch(arr, x) {
+function BinarySearch(array, searchKey, start, end) {
 
-  let start = 0, end = arr.length - 1;
+  if (start > end) return false; 
+ 
+  let mid=Math.floor((start + end)/2); 
+ 
+  if (array[mid]===searchKey) return array[mid]; 
+        
+  if(array[mid] > searchKey)  
+    return BinarySearch(array, searchKey, start, mid-1); 
+  else
 
-
-
-  while (start <= end) {
-    for (let i = 0; i <= arr.length; i++) {
-
-      let mid = Math.floor((start + end) / 2);
-
-      if (arr[mid] === x) return arr[mid];
-
-      else if (arr[mid] < x)
-        start = mid + 1;
-      else
-        end = mid - 1;
-    }
-  }
-
-  return -1;
-
-}
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-
-BinarySearch(arr, 1);
-BinarySearch(arr, 111);
+    return BinarySearch(array, searchKey, mid+1, end); 
+} 
+console.log(BinarySearch);
+// 
 
