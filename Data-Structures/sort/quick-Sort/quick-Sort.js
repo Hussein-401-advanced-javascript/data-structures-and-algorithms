ee51bfef1d3d5f6c8a2a59e72347adfa0739271a
hussein <hussein66253@icloud.com>
2020-10-13 21:47:05 +0300

    CC28 DONE

---
+++ Data-Structures/sort/quick-Sort/quick-Sort.js
@@ -0,0 +1,31 @@
+function quickSort(arr, left, right){
+    if (left < right){
+      let position = partition(arr, left, right);
+      quickSort(arr,left,position- 1);
+      quickSort(arr,position +1 ,right);
+    }
+    return arr;
+  }
+  function partition(arr, left, right){
+    let pivot = arr[right];
+    let low = left -1;
+    // console.log(low);
+    for(let i = left ; i < right; i++){
+      if(arr[i] <= pivot){
+        low++;
+        swap(arr,i,low);
+      }
+    }
+    swap(arr,right ,low +1);
+    return low +1;
+  }
+  function swap(arr, i, low){
+    let temp ;
+    temp = arr[i];
+    arr[i] = arr[low];
+    arr[low] = temp;
+    
+  }
+  let arr = [5,7,8,2,1,4,9,10,3]; 
+  quickSort(arr, 0 , 8);
+  module.exports = quickSort;
